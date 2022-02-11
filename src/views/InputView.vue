3142d2177f989a403ab591d9a77e4b6a952dcc67
Imam Taufiqurrokhman <imamtaufiqurrokhman@gmail.com>
2022-02-11 14:50:45 +0700

    fix input view layout and add data +  method

--- src/views/InputView.vue
+++ src/views/InputView.vue
@@ -2,29 +2,25 @@
     <div class="px-4 py-5 my-5r">
         <div class="card">
             <div class="card-header">
-                <h1 class="display-5 fw-bold text-cente">Input</h1>
+                <h1 class="display-5 fw-bold text-cente">Input Phone Number</h1>
             </div>
             <div class="card-body">
                 <div class="mx-auto">
-                    <!-- <p class="lead mb-4">Aplikasi ini dibuat dengan menggunakan node js sebagai runtim</p> -->
-                    <div class="d-grid gap-2 d-sm-flex">
-                        <form>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
-                            </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" class="btn btn-success m-2">Save</button>
-                            <button type="submit" class="btn btn-primary m-2">Auto</button>
-                        </form>
+                    <div class="row mb-3">
+                        <label for="inputPhoneNumber" class="form-label">Phone Number</label>
+                        <input v-model="inputData.number" type="phone" class="form-control" id="inputPhoneNumber">
+                    </div>
+                    <div class="row mb-3">
+                        <label for="selectProvider" class="form-label">Provider</label>
+                        <select v-model="inputData.provider_id" class="form-control" id="selectProvider">
+                            <option v-bind:key="provider.id" v-for="provider in providers" :value="provider.id" v-text="provider.name"/>
+                        </select>
+                    </div>
+                    <div class="row">
+                        <div class="col m-l-auto">
+                            <button @click="storeData()" class="btn btn-success m-2">Save</button>
+                            <button @click="autoCreateData()" class="btn btn-primary m-2">Auto</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -34,11 +30,67 @@
 <script>
 export default{
     name: "InputView",
+    data() {
+        return {
+            inputData: {
+                number: null,
+                provider_id: 0
+            },
+            providers : [
+                {
+                    id: 0,
+                    name: 'Select Provider'
+                }
+            ]
+        }
+    },
+    methods: {
+        getProviderData() {
+            this.http.get('/provider')
+            .then(response => {
+                this.providers.push(...response.data.msg)
+                console.log(response.data)
+            })
+            .catch(error => {
+                // alert("Error")
+                console.log(error)
+            })
+        },
+        storeData() {
+            if(this.inputData.number && this.inputData.provider_id) {
+                this.http.post('/phone_number', this.inputData)
+                .then(response => {
+                    console.log(response.data)
+                    alert('Data telah disimpan.')
+                    this.$router.push('/output')
+                })
+                .catch(error => {
+                    alert("Error")
+                    console.log(error)
+                })
+            } else {
+                alert('Phone number and provider must no be empty!')
+            }
+        },
+        autoCreateData() {
+            this.http.post('/phone_number/auto', this.inputData)
+            .then(response => {
+                alert('Data telah disimpan.')
+                console.log(response.data)
+                this.$router.push('/output')
+            })
+            .catch(error => {
+                alert("Error")
+                console.log(error)
+            })
+        }
+    },
     inject: ['Vue3GoogleOauth'],
     created () {
         if (!this.Vue3GoogleOauth.isAuthorized) {
             this.$router.push('/')
         }
+        this.getProviderData()
     }
 }
 </script>
