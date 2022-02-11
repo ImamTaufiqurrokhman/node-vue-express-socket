f8b88a5d7690aa0136a34467f647541691898400
Imam Taufiqurrokhman <imamtaufiqurrokhman@gmail.com>
2022-02-11 14:51:39 +0700

    add axios and change gauthclient id

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import gAuthPlugin from 'vue3-google-oauth2';
+import axios from "axios";
 
 const app = createApp(App)
 
-// let gauthClientId = "768834812579-ivi0oopbkqe05cg6t41p83t7gteekut6.apps.googleusercontent.com";
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:3000/api"
+});
+
 let gauthClientId = "1045846826707-p0vu22ae02l401vdf90i77ggp0ia4do8.apps.googleusercontent.com";
 
 app.use(gAuthPlugin, { clientId: gauthClientId, scope: 'email', prompt: 'consent', fetch_basic_profile: false })
 app.use(router)
+app.config.globalProperties.http = axiosInstance
 
 app.mount('#app')
